64cddd7a83c691a8e7801cd1fa921129deb3622c
PJH <darkstry@naver.com>
2016-10-04 10:43:06 +0900

    First code bit

---
+++ Lab4_Git/src/Calculator.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Calculator {
+
+	//Leader
+	public static void main(String[] args) {
+
+	}
+	
+	//Programmer A
+	public double add(double x, double y){
+		return 0;
+	}
+	
+	//Programmer B
+	public double subtract(double x, double y){
+		return 0;
+	}
+	
+	//Programmer A
+	public double multiply(double x, double y){
+		return 0;
+	}
+	
+	//Programmer B
+	public double divide(double x, double y){
+		return 0;
+	}
+	
+	//Programmer B
+	public double remainder(double x, double y){
+		return 0;
+	}
+	
+	//Programmer A
+	public double toString(double x, double y){
+		return 0;
+	}
+
+}
