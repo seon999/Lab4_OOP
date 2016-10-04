cf3a831223610f4b6c417929f8bf691d75655487
SUNGMIN <SUNGMIN@sungmin>
2016-10-04 11:23:13 +0900

    ProgrammerA commit

--- Lab4_Git/src/Calculator.java
+++ Lab4_Git/src/Calculator.java
@@ -4,12 +4,14 @@ public class Calculator {
 
 	//Leader
 	public static void main(String[] args) {
-
+		
+		
 	}
 	
 	//Programmer A
 	public double add(double x, double y){
-		return 0;
+		double result = x+y;
+		return result;
 	}
 	
 	//Programmer B
@@ -19,7 +21,8 @@ public class Calculator {
 	
 	//Programmer A
 	public double multiply(double x, double y){
-		return 0;
+		double result = x*y;
+		return result;
 	}
 	
 	//Programmer B
@@ -34,7 +37,17 @@ public class Calculator {
 	
 	//Programmer A
 	public String toString(){
-		return null;
+		
+		String Leader = "Choi SeonHo, 201120978";
+		String ProgrammerA = "Yoo Sungmin , 201120917";
+		String ProgrammerB = "Ok Gyeonghwan, 201320884";
+		String URI = "https://github.com/seon999/Lab4_OOP.git";
+		String tostring = "Member name, IDs : " + Leader + "\n\t\t" + ProgrammerA + "\n\t\t" + ProgrammerB 
+				+ "\nURI : " + URI;
+		System.out.print(tostring);
+		
+		return tostring;
 	}
 
 }
+
